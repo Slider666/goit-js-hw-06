0e8c78bf4ad1db19cc500b13bfeea83d54a107cb
Slider666 <112625010+Slider666@users.noreply.github.com>
2022-10-31 17:06:09 +0200

    new-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const render = document.querySelector('[data-action="render"]');
+const destroy = document.querySelector('[data-action="destroy"]');
+const boxes = document.getElementById('boxes');
