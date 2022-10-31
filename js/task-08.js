14ad42b6fc2dd623c3003544ec6caa536961aeba
Slider666 <112625010+Slider666@users.noreply.github.com>
2022-10-31 15:24:39 +0200

    new-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
+const formObject = {};
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  // const formData = new FormData(event.currentTarget);
+
+  // formData.forEach(email, password) => {
+  //   console.log(email, password)
+  // }
+
+  if (email === '' || password === '') {
+    return alert('Заполните все поля');
+  }
+  (formObject.email = email), (formObject.password = password);
+  event.currentTarget.reset();
+
+  console.log(formObject);
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorButton = document.querySelector('.change-color');
+const text = document.querySelector('.color');
+
+colorButton.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  text.textContent = getRandomHexColor();
+});
